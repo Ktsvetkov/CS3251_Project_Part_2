cf2325a0efd1565de49efa9d1bae78400b2b44a4
ktsvetkov@gatech.edu <ktsvetkov@gatech.edu>
2016-04-20 17:12:53 -0400

    only send closereceiver if sender window has been cleared by closesender

--- RTPSocket.py
+++ RTPSocket.py
@@ -197,10 +197,11 @@ class RTPSocket:
             print "\n Current Data Confirmation: " + str(self.dataToSendConfirmation) + "\n"
 
     def resendCloseReceiver(self):
-        packetToSend = RTPPacket(self.outgoingConnectionIP, self.outgoingConnectionPort, self.srcIP, self.portNumber, "closereceiver", 0, 0, self.dataToSendName)
-        self.socketManager.sendPacket(packetToSend)
-        self.closeReceiverTimer = threading.Timer(.2, self.resendCloseReceiver)
-        self.closeReceiverTimer.start()
+        if len(self.dataToSendConfirmation) != 0:
+            packetToSend = RTPPacket(self.outgoingConnectionIP, self.outgoingConnectionPort, self.srcIP, self.portNumber, "closereceiver", 0, 0, self.dataToSendName)
+            self.socketManager.sendPacket(packetToSend)
+            self.closeReceiverTimer = threading.Timer(.2, self.resendCloseReceiver)
+            self.closeReceiverTimer.start()
 
 
 ########################################
